6a010f2a4057878d3f72075533260d40dc5360c9
aeff60 <ssbunklang@gmail.com>
2023-09-20 20:02:57 +0700

    Add image feature and fix issue with opening from Public URLs

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 players = {}
+games = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('create_game')
+def create_game():
+    session_id = request.sid
+    games[session_id] = {'players': {session_id: {'x': 200, 'y': 200, 'dir': 'RIGHT'}}}
+    join_room(session_id)
+    emit('game_created', {'game_id': session_id}, room=session_id)
+
+@socketio.on('join_game')
+def join_game(data):
+    game_id = data['game_id']
+    session_id = request.sid
+    if game_id in games:
+        games[game_id]['players'][session_id] = {'x': 200, 'y': 200, 'dir': 'RIGHT'}
+        join_room(game_id)
+        emit('update_game', {'players': games[game_id]['players']}, room=game_id)
+
 @socketio.on('connect')
 def handle_connect():
     session_id = request.sid
     players[session_id] = {
-        'x': 200,  # Assuming canvas width is 400
-        'y': 200,  # Assuming canvas height is 400
+        'x': 200,  
+        'y': 200,
         'dir': 'RIGHT'
     }
     emit('update_game', {'players': players})
@@ -35,4 +53,4 @@ def handle_disconnect():
     emit('update_game', {'players': players}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=False, host='0.0.0.0', port=5000)
--- static/js/game.js
+++ static/js/game.js
@@ -1,26 +1,66 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+const socket = io.connect(`${protocol}://${document.domain}:${location.port}`);
 
-const PACMAN_SIZE = 20;
+const PACMAN_SIZE = 30;
 const DOT_SIZE = 5;
-const SPEED = 5;
+const SPEED = 10;
 
 let players = {};
+let imageCache = {};
+let dots = initializeDots();
 
-let dots = [];
-for (let i = 0; i < canvas.width; i += 40) {
-    for (let j = 0; j < canvas.height; j += 40) {
-        dots.push({ x: i + 20, y: j + 20 });
+function initializeDots() {
+    let dotsArray = [];
+    for (let i = 0; i < canvas.width; i += 40) {
+        for (let j = 0; j < canvas.height; j += 40) {
+            dotsArray.push({ x: i + 20, y: j + 20 });
+        }
     }
+    return dotsArray;
+}
+
+socket.on('game_created', handleGameCreated);
+socket.on('update_game', data => players = data.players);
+
+document.addEventListener('keydown', handlePlayerMovement);
+setInterval(updateGame, 500 / 60);
+
+function handleGameCreated(data) {
+    alert('Game created! Share this ID with your friend: ' + data.game_id);
+    document.getElementById('lobby').style.display = 'none';
+    document.getElementById('gameCanvas').style.display = 'block';
+}
+
+function createGame() {
+    socket.emit('create_game');
+}
+
+function joinGame() {
+    const gameId = document.getElementById('gameIdInput').value;
+    socket.emit('join_game', { 'game_id': gameId });
 }
 
 function drawAllPacmans() {
     for (let id in players) {
-        let p = players[id];
-        ctx.fillStyle = "green";
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, PACMAN_SIZE, 0, Math.PI * 2);
-        ctx.fill();
+        const p = players[id];
+        drawPacmanImage(p);
+    }
+}
+
+function drawPacmanImage(p) {
+    const imageUrl = p.imageUrl;
+
+    if (!imageCache[imageUrl]) {
+        const playerImage = new Image();
+        playerImage.src = imageUrl;
+        playerImage.onload = function() {
+            imageCache[imageUrl] = playerImage;
+            ctx.drawImage(playerImage, p.x - PACMAN_SIZE, p.y - PACMAN_SIZE, PACMAN_SIZE * 2, PACMAN_SIZE * 2);
+        }
+    } else {
+        ctx.drawImage(imageCache[imageUrl], p.x - PACMAN_SIZE, p.y - PACMAN_SIZE, PACMAN_SIZE * 2, PACMAN_SIZE * 2);
     }
 }
 
@@ -39,35 +79,33 @@ function updateGame() {
     drawAllPacmans();
 }
 
-const socket = io.connect('http://' + document.domain + ':' + location.port);
-
-socket.on('update_game', function(data) {
-    players = data.players;
-});
-
-document.addEventListener('keydown', (e) => {
+function handlePlayerMovement(e) {
     let player = players[socket.id] || {};
-    
-    if (e.key === 'ArrowRight') player.dir = 'RIGHT';
-    if (e.key === 'ArrowLeft') player.dir = 'LEFT';
-    if (e.key === 'ArrowUp') player.dir = 'UP';
-    if (e.key === 'ArrowDown') player.dir = 'DOWN';
-    
-    if (player.dir === 'RIGHT') player.x += SPEED;
-    if (player.dir === 'LEFT') player.x -= SPEED;
-    if (player.dir === 'UP') player.y -= SPEED;
-    if (player.dir === 'DOWN') player.y += SPEED;
-    
+    movePlayerBasedOnKey(e.key, player);
+
     for (let i = 0; i < dots.length; i++) {
-        let dot = dots[i];
-        let distance = Math.sqrt(((player.x - dot.x) ** 2) + ((player.y - dot.y) ** 2));
-        if (distance < PACMAN_SIZE + DOT_SIZE) {
+        if (isPlayerCloseToDot(player, dots[i])) {
             dots.splice(i, 1);
             i--;
         }
     }
 
-    socket.emit('player_move', { pacman: player });
-});
+    const imageUrl = document.getElementById('pacmanImageUrl').value;
+    socket.emit('player_move', { pacman: { ...player, imageUrl: imageUrl } });
+}
 
-setInterval(updateGame, 1000 / 60);
+function movePlayerBasedOnKey(key, player) {
+    const movementMap = {
+        'ArrowRight': () => player.x += SPEED,
+        'ArrowLeft': () => player.x -= SPEED,
+        'ArrowUp': () => player.y -= SPEED,
+        'ArrowDown': () => player.y += SPEED
+    };
+    const move = movementMap[key];
+    if (move) move();
+}
+
+function isPlayerCloseToDot(player, dot) {
+    const distance = Math.sqrt(((player.x - dot.x) ** 2) + ((player.y - dot.y) ** 2));
+    return distance < PACMAN_SIZE + DOT_SIZE;
+}
